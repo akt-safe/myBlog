f5f05d1c61a2b2ed995d954d89421a47e9467535
Urua <24709418+UruaUru@users.noreply.github.com>
2021-11-18 01:12:19 +0900

    space typo ESLintを入れた

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,12 +1,12 @@
-import { client } from '../../libs/client';
-import styles from '../../styles/Home.module.scss';
+import { client } from "../../libs/client";
+import styles from "../../styles/Home.module.scss";
 
 export default function BlogId({ blog }) {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>{blog.title}</h1>
       <p className={styles.publishedAt}>{blog.publishedAt}</p>
-      <p className="category">{blog.category&&`${blog.category.name}`}</p>
+      <p className="category">{blog.category && `${blog.category.name}`}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
