e1766c83726235d66fccf16267826e954137d2e3
Urua <24709418+UruaUru@users.noreply.github.com>
2021-11-18 00:49:35 +0900

    nextの動作設定を追加

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  // 出力先指定
+  distDir: "build",
+  // Reactが実行して警告する一連のチェックを有効にする組み込みコンポーネント。trueが公式推奨
   reactStrictMode: true,
-}
+  // レスポンスに付与されるX-Powered-Byの出力設定
+  poweredByHeader: false,
+};
